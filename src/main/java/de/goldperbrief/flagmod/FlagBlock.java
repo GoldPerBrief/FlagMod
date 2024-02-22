c9b94359ae4b4bef8fc19edf480efc4d438da2dc
Minkampel <emil@grasskamp.de>
2024-02-22 18:45:24 +0100

    Add the Block thingy

---
+++ src/main/java/de/goldperbrief/flagmod/FlagBlock.java
@@ -0,0 +1,26 @@
+package de.goldperbrief.flagmod;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class FlagBlock extends Block {
+	public FlagBlock(Settings settings) {
+		super(settings);
+	}
+
+	@Override
+	public ActionResult onUse(BlockState state, World world, BlockPos blockPos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+		if (!world.isClient) {
+			player.sendMessage(Text.literal("Hello, world!"), false);
+		}
+
+		return ActionResult.SUCCESS;
+	}
+}
--- src/main/java/de/goldperbrief/flagmod/FlagMod.java
+++ src/main/java/de/goldperbrief/flagmod/FlagMod.java
@@ -1,7 +1,14 @@
 package de.goldperbrief.flagmod;
 
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.quiltmc.qsl.block.extensions.api.QuiltBlockSettings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,10 +16,13 @@ public class FlagMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod name as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("Example Mod");
+	public static final Logger LOGGER = LoggerFactory.getLogger("Flag Mod");
+	public static final Block FLAG_BLOCK = new Block(QuiltBlockSettings.create().strength(4.0f));
 
 	@Override
 	public void onInitialize(ModContainer mod) {
 		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
+		Registry.register(Registries.BLOCK, new Identifier("flag_mod", "flag_block"), FLAG_BLOCK);
+		Registry.register(Registries.ITEM, new Identifier("flag_mod", "flag_block"), new BlockItem(FLAG_BLOCK, new Item.Settings()));
 	}
 }
