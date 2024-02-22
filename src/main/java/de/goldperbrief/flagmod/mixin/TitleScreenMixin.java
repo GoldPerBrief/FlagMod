b455255c2df2bb351037cd9137eff930ea5cc850
Minkampel <emil@grasskamp.de>
2024-02-22 17:43:00 +0100

    Updated some Thingies

--- src/main/java/de/goldperbrief/flagmod/mixin/TitleScreenMixin.java
+++ src/main/java/de/goldperbrief/flagmod/mixin/TitleScreenMixin.java
@@ -1,6 +1,6 @@
-package com.example.example_mod.mixin;
+package de.goldperbrief.flagmod.mixin;
 
-import com.example.example_mod.ExampleMod;
+import de.goldperbrief.flagmod.FlagMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class TitleScreenMixin {
 	@Inject(method = "init", at = @At("TAIL"))
 	public void onInit(CallbackInfo ci) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		FlagMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
